ce7732d06bfac0a029311798c70ce1926fb761da
doktordirk <doktordirk@outlook.de>
2016-04-30 19:17:38 +0200

    feat(client-ts): add typescript sample

---
+++ skeleton-typescript/src/main.ts
@@ -0,0 +1,34 @@
+import 'bootstrap';
+import {Aurelia} from 'aurelia-framework';
+
+import * as Backend from 'i18next-xhr-backend'; // <-- your previously installed backend plugin
+console.log(Backend)
+
+export function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging()
+    .plugin('aurelia-i18n', (instance) => {
+      // register backend plugin
+      instance.i18next.use(Backend);
+
+      // adapt options to your needs (see http://i18next.com/docs/options/)
+      // make sure to return the promise of the setup method, in order to guarantee proper loading
+      return instance.setup({
+        backend: {                                  // <-- configure backend settings
+          loadPath: '/locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+        },
+        lng : 'de',
+        attributes : ['t','i18n'],
+        fallbackLng : 'en',
+        debug : false
+      });
+    });
+  //Uncomment the line below to enable animation.
+  //aurelia.use.plugin('aurelia-animator-css');
+
+  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  //aurelia.use.plugin('aurelia-html-import-template-loader')
+
+  aurelia.start().then(() => aurelia.setRoot());
+}
