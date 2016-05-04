43f6b6516760d8b0ef0fe1af6cfddeb3b8b34c02
doktordirk <doktordirk@outlook.de>
2016-05-04 18:36:06 +0200

    feat(es6): add es2016 sample

--- skeleton-es2016/src/main.js
+++ skeleton-es2016/src/main.js
@@ -1,15 +1,14 @@
 import 'bootstrap';
-import {Aurelia} from 'aurelia-framework';
 
-import * as Backend from 'i18next-xhr-backend'; // <-- your previously installed backend plugin
+import XHR from 'i18next-xhr-backend'; // <-- your previously installed backend plugin
 
-export function configure(aurelia: Aurelia) {
+export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
     .plugin('aurelia-i18n', (instance) => {
       // register backend plugin
-      instance.i18next.use(Backend);
+      instance.i18next.use(XHR);
 
       // adapt options to your needs (see http://i18next.com/docs/options/)
       // make sure to return the promise of the setup method, in order to guarantee proper loading
@@ -23,8 +22,10 @@ export function configure(aurelia: Aurelia) {
         debug : false
       });
     });
+
   //Uncomment the line below to enable animation.
   //aurelia.use.plugin('aurelia-animator-css');
+  //if the css animator is enabled, add swap-order="after" to all router-view elements
 
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   //aurelia.use.plugin('aurelia-html-import-template-loader')
